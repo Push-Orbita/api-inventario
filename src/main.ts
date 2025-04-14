4fdb6691a80d93689ea15ba09701c2af5dbb1e46
silfarias <silvifarias17@gmail.com>
2025-04-14 15:02:21 -0300

    feat: configuración de variables de entorno con ConfigModule

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,7 +17,7 @@ async function bootstrap() {
   );
 
   setupSwagger(app);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(configService.get('PORT')!);
 }
 
 bootstrap();
