334ca9a2897b7b73515a0445473ad77bfefcec19
Calu <lucas_britez@hotmail.com>
2025-04-29 00:03:25 -0300

    ya funciona Get y Patch en Unidad

--- src/schematics/unidad/unidad.controller.ts
+++ src/schematics/unidad/unidad.controller.ts
@@ -1,30 +1,86 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { UnidadService } from './unidad.service';
 import { CreateUnidadRequestDto } from './dto/create-unidad-request.dto';
 import { UpdateUnidadRequestDto } from './dto/update-unidad-request.dto';
+import { UnidadDTO } from './dto/unidad.dto';
+import { ApiBadRequestResponse, ApiBody, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam } from '@nestjs/swagger';
 
 @Controller('unidad')
 export class UnidadController {
   constructor(private readonly unidadService: UnidadService) {}
 
   @Post()
-  create(@Body() createUnidadDto: CreateUnidadRequestDto) {
-    return this.unidadService.create(createUnidadDto);
+  @ApiOperation({
+      summary: 'Crear una nueva unidad',
+      description: 'Permite crear una nueva unidad con los datos proporcionados.',
+    })
+    @ApiBody({
+      type: CreateUnidadRequestDto,
+      description: 'Datos de la nueva unidad a crear.',
+    })
+    @ApiOkResponse({
+      type: UnidadDTO,
+      description: 'Unidad creada correctamente.',
+    })
+    @ApiBadRequestResponse({
+      description: 'Solicitud incorrecta o datos inválidos.',
+    })
+  create(@Body() createUnidadRequestDto: CreateUnidadRequestDto): Promise<UnidadDTO> {
+    return this.unidadService.create(createUnidadRequestDto);
+  }
+
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Obtener detalles de una unidad',
+    description: 'Obtiene los detalles de una unidad en específico mediante su ID.',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'Id de la unidad',
+    type: 'number',
+  })
+  @ApiOkResponse({
+    type: UnidadDTO,
+    description: 'Detalles de la unidad obtenidos correctamente.',
+  })
+  @ApiBadRequestResponse({ description: 'Solicitud incorrecta.' })
+  @ApiNotFoundResponse({ description: 'Unidad no encontrada.' })
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<UnidadDTO> {
+    return this.unidadService.findById(id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Actualizar una unidad existente',
+    description: 'Permite actualizar los datos de una unidad existente mediante su ID.',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'ID de la unidad a actualizar.',
+  })
+  @ApiBody({
+    type: UpdateUnidadRequestDto,
+    description: 'Datos de la unidad a actualizar.',
+  })
+  @ApiOkResponse({
+    type: UnidadDTO,
+    description: 'Unidad actualizada correctamente.',
+  })
+  @ApiBadRequestResponse({
+    description: 'Solicitud incorrecta o datos inválidos.',
+  })
+  @ApiNotFoundResponse({
+    description: 'Unidad no encontrada.',
+  })
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateUnidadRequestDto: UpdateUnidadRequestDto): Promise<UnidadDTO> {
+    return this.unidadService.update(id, updateUnidadRequestDto);
   }
 
   @Get()
   findAll() {
     return this.unidadService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.unidadService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUnidadDto: UpdateUnidadRequestDto) {
-    return this.unidadService.update(+id, updateUnidadDto);
   }
 
   @Delete(':id')
--- src/schematics/unidad/unidad.service.ts
+++ src/schematics/unidad/unidad.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateUnidadRequestDto } from './dto/create-unidad-request.dto';
 import { UpdateUnidadRequestDto } from './dto/update-unidad-request.dto';
 import { UnidadDTO } from './dto/unidad.dto';
@@ -31,16 +31,55 @@ export class UnidadService {
     }
   }
 
+  public async findById(id: number): Promise<UnidadDTO> {
+    try {
+      const unidad = await this.unidadRepository.findOne({
+        where: { id: id },
+        relations: {
+          producto: true,
+          ubicacion: true,
+        },
+      });
+      if (!unidad) {
+        throw new NotFoundException(`No se encontró la unidad con id ${id}`);
+      }
+      return this.unidadMapper.entity2DTO(unidad);
+    } catch (error) {
+      throw new BadRequestException(
+        `Error al buscar unidad: ${error.message}`,
+      );
+    }
+  }
+
+  public async update(id: number, updateUnidadRequestDto: UpdateUnidadRequestDto): Promise<UnidadDTO> {
+    try {
+      const unidad = await this.unidadRepository.findOne({ where: { id: id } });
+      if (!unidad) throw new NotFoundException(`No se encontró la unidad con id ${id}`);
+      const updateUnidad = await this.unidadMapper.updateDTO2Entity(unidad, updateUnidadRequestDto);
+      await this.unidadRepository.save(updateUnidad);
+      const unidadUpdate = await this.unidadMapper.entity2DTO(updateUnidad);
+      return unidadUpdate;
+    } catch (error) {
+      throw new BadRequestException(
+        `Error al actualizar una unidad: ${error.message}`,
+      );
+    }
+  }
+
+
+
+
+
+
+
+
+
   findAll() {
     return `This action returns all unidad`;
   }
 
   findOne(id: number) {
     return `This action returns a #${id} unidad`;
-  }
-
-  update(id: number, updateUnidadDto: UpdateUnidadRequestDto) {
-    return `This action updates a #${id} unidad`;
   }
 
   remove(id: number) {
