c94d068c04097c3679da06af6eba2adf4596a7ed
silfarias <silvifarias17@gmail.com>
2025-05-08 09:52:50 -0300

    refactor: revisando entidades

--- src/schematics/movimiento/entities/movimiento.entity.ts
+++ src/schematics/movimiento/entities/movimiento.entity.ts
@@ -6,19 +6,10 @@ import { TipoOperacionEnum } from "src/common/enums/tipo-operacion.enum";
 @Entity({ name:'inve_08_mov_movimiento'})
 export class Movimiento extends BaseEntity{
 
-  // Persona
-  @Column({ name: 'inve08_persona', unique: true, nullable: false })
-  persona: string;
-
-  // Unidad
-  @ManyToOne(() => Unidad)
-  @JoinColumn({ name: 'rela_inve06'})
-  unidad: Unidad;
-
   // Fecha de movimiento
   @Column({ name: 'inve08_fecha' })
   fecha: Date;
-
+  
   // Operacion
   @Column({
     name: 'inve08_operacion',
@@ -27,21 +18,24 @@ export class Movimiento extends BaseEntity{
     nullable: false
   })
   operacion: TipoOperacionEnum;
-
+  
   // Detalle
   @Column({ name: 'inve08_detalle', nullable: true })
   detalle: string;
+
+  // Persona: relación
+  @Column({ name: 'inve08_persona', unique: true, nullable: false })
+  persona: string;
+
+  // Unidad
+  @ManyToOne(() => Unidad)
+  @JoinColumn({ name: 'rela_inve06'})
+  unidad: Unidad;
   
   static fromId(id: number) {
       const movimiento = new Movimiento();
       movimiento.id = id;
       return movimiento;
   }
-
-
-
-
-
-
 
 }
